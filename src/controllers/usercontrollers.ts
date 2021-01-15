a226a22540c5d960299afd123f7bd33cd11eac97
matheusF99 <mc79846@gmail.com>
2021-01-14 22:55:02 -0300

    buscando os usuarios no banco

--- src/controllers/usercontrollers.ts
+++ src/controllers/usercontrollers.ts
@@ -1,7 +1,9 @@
 import {Request, Response} from 'express'
-
+import User from '../schemas/user'
 class UserController{
-    public class async index(req:Request, res: Response) {
-    
+    public async index(req:Request, res: Response): Promise<string>{
+        
     }
 }
+
+export default new UserController()
