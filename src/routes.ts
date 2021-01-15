74bbcf00055358f9ec09ab8fa379521c7c997328
matheusF99 <mc79846@gmail.com>
2021-01-14 23:13:44 -0300

    ajustando o schema

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,11 @@ import {Router} from 'express'
 
 
 import UserController from './controllers/userControllersIndex'
+import UserControllerCreate from './controllers/userControllersCreate'
 
 const routes = Router()
 
 routes.get('/users', UserController.index)
+routes.post('/users', UserControllerCreate.create)
 
 export default routes
