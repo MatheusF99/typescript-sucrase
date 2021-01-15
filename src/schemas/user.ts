d9ad2bf381500624ddb520a14c4d08bf17965b77
matheusF99 <mc79846@gmail.com>
2021-01-14 23:16:30 -0300

    tipando os dados do schema

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,5 +1,11 @@
 import { Schema, model } from 'mongoose'
 
+interface UserProps{
+    email?: string
+    firtname: string
+    lastname: string
+}
+
 const UserSchema = new Schema({
     email: String,
     firstName: String,
